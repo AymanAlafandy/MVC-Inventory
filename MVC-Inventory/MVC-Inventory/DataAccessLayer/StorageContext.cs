3500870c1ca56405601bd7d967120edcea7f5e4a
Deidre-Festus <deidrefestus@hotmail.com>
2017-11-29 14:10:44 +0100

    Halfway mark

---
+++ MVC-Inventory/MVC-Inventory/DataAccessLayer/StorageContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace MVC_Inventory.DataAccessLayer
+{
+    public class StorageContext : DbContext
+    {
+        //MVC-Inventory is name of our Database
+        public StorageContext() : base("MVC-Inventory")
+        { }
+
+        //Everything in our DB needs to be a public DbSet property.  
+        //It sets something in the db
+        //Each dbset becomes a table.  Here we create the Product table
+        public DbSet<Models.Product> Products{get;set;}
+
+
+
+    }
+}
