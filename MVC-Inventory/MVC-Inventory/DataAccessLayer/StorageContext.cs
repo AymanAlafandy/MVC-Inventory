55e9194cf58bcd70cb9169b6b3c405ada14ca922
priya tadi <priya95tadi@hotmail.com>
2017-11-29 14:11:43 +0100

    HalfMVC

---
+++ MVC-Inventory/MVC-Inventory/DataAccessLayer/StorageContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVC_Inventory.DataAccessLayer
+{
+    public class StorageContext : DbContext
+    {
+        public StorageContext() : base("MVC-Inventory")
+        {  }
+
+        public DbSet<Models.Product> Products {get;set;}
+    }
+}
