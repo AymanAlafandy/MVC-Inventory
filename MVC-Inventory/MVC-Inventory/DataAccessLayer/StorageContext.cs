8f254327856249fc0453e002c562cac0cf821f9d
swapna <pasupuletisaiswapna85@gmail.com>
2017-11-29 14:13:07 +0100

    commiting after doing exclude 3 init files

---
+++ MVC-Inventory/MVC-Inventory/DataAccessLayer/StorageContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;                                                              // to get this right click on references folder->add references (or) still if you dont get it,go to tools ->Nuget Package Manager->  Manage Nuget...->search for Entity in the Browse->check the project->select latest version and install
+
+namespace MVC_Inventory.DataAccessLayer
+{
+    public class StorageContext : DbContext
+    {
+        public StorageContext() : base("MVC-Inventory")                     //this string in bet base(" ") ->name of your data base
+        { }
+
+
+        public DbSet<Models.Product> Products { get; set; }                //property created->dbset->set something in to our database,here we are creating table "Product for product properties" in the database
+
+    }
+}
