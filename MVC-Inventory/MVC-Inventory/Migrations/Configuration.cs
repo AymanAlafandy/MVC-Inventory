e9bab73a195061967fcfa027bc2f213820f6ca20
Taleb01 <taleb_n1@hotmail.com>
2017-11-29 16:30:43 +0100

    uppdate nav. add electronics

--- MVC-Inventory/MVC-Inventory/Migrations/Configuration.cs
+++ MVC-Inventory/MVC-Inventory/Migrations/Configuration.cs
@@ -32,6 +32,20 @@ namespace MVC_Inventory.Migrations
             context.Products.AddOrUpdate( n => n.Name, 
                 new Product() { Name="Toaster", Category="Electronics", Count= 0,
                     Price = 200, Shelf="E3", Description="It's a toaster"});
-        }
+
+            context.Products.AddOrUpdate(n => n.Name,
+                new Product() { Name = "Oven", Category = "Electronics", Count = 0,
+                Price = 400, Shelf = "A4", Description = "It's a Oven"});
+
+            context.Products.AddOrUpdate(n => n.Name,
+                new Product() { Name = "Kiwi bird", Category = "Fruit", Count = 0,
+                Price = 2, Shelf = "F6", Description = "It's a fruit" });
+
+
+            context.Products.AddOrUpdate(n => n.Name,
+                new Product() { Name = "Kiwi Island", Category = "Island", Count = 1,
+                Price = int.MaxValue, Shelf = "F6", Description = "It's a small island" });
+
+        }                                                                    
     }
 }
